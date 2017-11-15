2e5b329b40be057c840e04838e6bcf5d3891c303
Kirill Bespalov <kbespalov@mirantis.com>
2017-11-15 19:56:21 +0300

    Refactor folders

--- scripts/mcp_tls_enable.sh
+++ scripts/mcp_tls_enable.sh
@@ -19,17 +19,7 @@ salt -C 'I@rabbitmq:server' state.sls rabbitmq
 
 # Workaround to restart Galera cluster
 
-salt -C 'I@galera:slave'  cmd.run "pkill -9 mysql" -b 1
-salt -C 'I@galera:master' cmd.run "pkill -9 mysql" -b 1
-
-# Start Galera Master
-salt -C 'I@galera:master' cmd.run "rm -rdf /var/run/mysqld"
-salt -C 'I@galera:master' cmd.run "mkdir /var/run/mysqld"
-salt -C 'I@galera:master' cmd.run "chown mysql:mysql /var/run/mysqld"
-salt -C 'I@galera:master' cmd.run "service mysql bootstrap"
-
-# Start Galera Slave's
-salt -C 'I@galera:slave' service.start mysql
+exec ./galera_restart.sh
 
 # Apply changes to openstack services
 
